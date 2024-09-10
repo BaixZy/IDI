7fcb9db59b16afab9ac98209cf5e50f3b790dc8c
BaixZy <135122424+BaixZy@users.noreply.github.com>
2024-09-10 09:55:08 +0200

    Add files via upload

---
+++ S0/esqueleto/ui_Form.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'Form.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.12
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_FORM_H
+#define UI_FORM_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MyForm
+{
+public:
+
+    void setupUi(QWidget *MyForm)
+    {
+        if (MyForm->objectName().isEmpty())
+            MyForm->setObjectName(QString::fromUtf8("MyForm"));
+        MyForm->resize(400, 300);
+
+        retranslateUi(MyForm);
+
+        QMetaObject::connectSlotsByName(MyForm);
+    } // setupUi
+
+    void retranslateUi(QWidget *MyForm)
+    {
+        MyForm->setWindowTitle(QCoreApplication::translate("MyForm", "Form", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MyForm: public Ui_MyForm {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_FORM_H
