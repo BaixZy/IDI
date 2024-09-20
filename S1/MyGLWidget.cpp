9c4f4bd8d41fc43df8819dcc90823d0f60a2239d
BaixZy <135122424+BaixZy@users.noreply.github.com>
2024-09-20 17:11:28 +0200

    Add files via upload
    Corregido

--- S1/MyGLWidget.cpp
+++ S1/MyGLWidget.cpp
@@ -29,18 +29,46 @@ void MyGLWidget::paintGL ()
 {
 // En cas de voler canviar els paràmetres del viewport, descomenteu la crida següent i
 // useu els paràmetres que considereu (els que hi ha són els de per defecte)
-//  glViewport (0, 0, ample, alt);
+ // glViewport (0, 0, ample, alt);
   
   glClear (GL_COLOR_BUFFER_BIT);  // Esborrem el frame-buffer
 
+  //Pintem la casa VAO1
+  
+  glViewport (0, 0, ample/2, alt/2);
   // Activem l'Array a pintar 
   glBindVertexArray(VAO1);
  
   // Pintem l'escena
-  glDrawArrays(GL_TRIANGLES, 0, 6);
+  glDrawArrays(GL_TRIANGLES, 0, 9);
   
   // Desactivem el VAO
   glBindVertexArray(0);
+  
+  glBindVertexArray(VAO2);
+  
+  glDrawArrays(GL_TRIANGLES, 0, 3);  sssssssssssss// 3 vértices para el triángulo
+  
+  glBindVertexArray(0); //Desactivem VAO
+  s
+  //Pintem el triangle VAO2
+  glViewport (ample/2, alt/2, ample/2, alt/2);
+  
+    // Activem l'Array a pintar 
+  glBindVertexArray(VAO1);
+ 
+  // Pintem l'escena
+  glDrawArrays(GL_TRIANGLES, 0, 9);
+  
+  // Desactivem el VAO
+  glBindVertexArray(0);
+  
+  glBindVertexArray(VAO2);
+  
+  glDrawArrays(GL_TRIANGLES, 0, 3);  // 3 vértices para el triángulo
+  
+  glBindVertexArray(0); //Desactivem VAO
+
 }
 
 void MyGLWidget::resizeGL (int w, int h)
@@ -59,16 +87,26 @@ void MyGLWidget::resizeGL (int w, int h)
 
 void MyGLWidget::creaBuffers ()
 {
-  glm::vec3 Vertices[6];  // Tres vèrtexs amb X, Y i Z
-  //Cuadrado
-  Vertices[0] = glm::vec3(-1.0, -1.0, 0.0);
-  Vertices[1] = glm::vec3(-1.0, 1.0, 0.0);
-  Vertices[2] = glm::vec3(1.0, -1.0, 0.0);
-  Vertices[3] = glm::vec3(-1.0, 1.0, 0.0);
-  Vertices[4] = glm::vec3(1.0, 1.0, 0.0);
-  Vertices[5] = glm::vec3(1.0, -1.0, 0.0);
-
+  glm::vec3 Vertices1[9];  // Tres vèrtexs amb X, Y i Z
+  //Casita -> VAO1
+  Vertices1[0] = glm::vec3(-1.0, -1.0, 0.0);
+  Vertices1[1] = glm::vec3(-1.0, 0.0, 0.0);
+  Vertices1[2] = glm::vec3(0.0, -1.0, 0.0);
+  Vertices1[3] = glm::vec3(-1.0, 0.0, 0.0);
+  Vertices1[4] = glm::vec3(0.0, 0.0, 0.0);
+  Vertices1[5] = glm::vec3(0.0, -1.0, 0.0);
+  Vertices1[6] = glm::vec3(-1.0, 0.0, 0.0);
+  Vertices1[7] = glm::vec3(0.0, 0.0, 0.0);
+  Vertices1[8] = glm::vec3(-0.5, 1.0, 0.0);
   
+  //Triángulo -> VAO2
+  glm::vec3 Vertices2[3];
+  Vertices2[0] = glm::vec3(0.0, 0.0, 0.0);
+  Vertices2[1] = glm::vec3(1.0, 0.0, 0.0);
+  Vertices2[2] = glm::vec3(0.5, 1.0, 0.0);
+
+
+  //1 VAO
   // Creació del Vertex Array Object (VAO) que usarem per pintar
   glGenVertexArrays(1, &VAO1); //Nos asignará un(indicado por el 1) VAO
   glBindVertexArray(VAO1); //Activame el VAO actual
@@ -77,13 +115,29 @@ void MyGLWidget::creaBuffers ()
   GLuint VBO1;
   glGenBuffers(1, &VBO1); //Nos asigna un VBO
   glBindBuffer(GL_ARRAY_BUFFER, VBO1); //Activa el VBO
-  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW); //Indico el bloque de datos a pasar a la GPU
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices1), Vertices1, GL_STATIC_DRAW); //Indico el bloque de datos a pasar a la GPU
   // Activem l'atribut que farem servir per vèrtex (només el 0 en aquest cas)	
   glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0); //Asignara 3 floats al vertexLoc(apuntador de la variable entrada)
   glEnableVertexAttribArray(vertexLoc); //Activamos
 
   // Desactivem el VAO
-  glBindVertexArray(0);
+  //glBindVertexArray(0); Al activar un VAO nuevo se desactiva el anterior.
+  
+  glGenVertexArrays(1, &VAO2); //Nos asignará un(indicado por el 1) VAO
+  glBindVertexArray(VAO2); //Activame el VAO actual
+  
+  GLuint VBO2;
+
+  // Creació del buffer amb les dades dels vèrtexs
+  glGenBuffers(1, &VBO2); //Nos asigna un VBO
+  glBindBuffer(GL_ARRAY_BUFFER, VBO2); //Activa el VBO
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices2), Vertices2, GL_STATIC_DRAW); //Indico el bloque de datos a pasar a la GPU
+  // Activem l'atribut que farem servir per vèrtex (només el 0 en aquest cas)	
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0); //Asignara 3 floats al vertexLoc(apuntador de la variable entrada)
+  glEnableVertexAttribArray(vertexLoc); //Activamos
+  
+  glBindVertexArray(0); //Desactivar VAO
+
 }
 
 void MyGLWidget::carregaShaders()
