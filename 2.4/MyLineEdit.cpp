5054b75b0c6e4a1e072df1196adba779086b0351
BaixZy <135122424+BaixZy@users.noreply.github.com>
2024-11-12 09:40:31 +0100

    Add files via upload

---
+++ 2.4/MyLineEdit.cpp
@@ -0,0 +1,14 @@
+#include "MyLineEdit.h"
+
+// constructor
+MyLineEdit::MyLineEdit(QWidget *parent)
+: QLineEdit(parent) {
+    connect(this,SIGNAL(returnPressed()),this,SLOT(tractaReturn()));
+    // Inicialització d'atributs si cal
+}
+
+// implementació slots
+void MyLineEdit::tractaReturn() {
+// Implementació de tractaReturn
+    emit returnPressed (text());
+}
